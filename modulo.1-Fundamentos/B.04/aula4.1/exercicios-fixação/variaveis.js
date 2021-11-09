cd1851fab5f762db6c0d53deba74941c56e39d4c
rafaelPermec <geekchef.dev@gmail.com>
2021-11-09 10:16:12 -0300

    :update: aula 1

---
+++ modulo.1-Fundamentos/B.04/aula4.1/exercicios-fixação/variaveis.js
@@ -0,0 +1,11 @@
+const myName = 'Rafael Perdigão Melo Castro';
+const birthCity = 'Belo Horizonte';
+var birthYear = 1990;
+
+    console.log(myName + " : " + birthCity + " : " + birthYear);
+
+var birthYear = 2030;
+    console.log (birthYear);
+
+const birthCity = 'São Paulo';
+    console.log(birthCity);
