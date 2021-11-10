547f8c56c241e968a844e769f51d2ffad5b8d0c6
rafaelPermec <geekchef.dev@gmail.com>
2021-11-10 19:02:35 -0300

    :tada: bloco finalizado

---
+++ modulo.1-Fundamentos/B.04/aula-4.2/exercicios-fixação/for.js
@@ -0,0 +1,21 @@
+/* Exercício 1
+    (for)
+
+Utilize o for para imprimir os elementos da lista groceryList com o console.log() : */
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+    for (var i = 0; i < groceryList.length; i +=1){
+        console.log(groceryList[i]);
+    }
+
+/* Exercício 2
+    (for/of)
+
+Utilize o for/of para imprimir os elementos da lista names com o console.log() : */
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+    for (nome of names){
+        console.log(nome)
+    }
