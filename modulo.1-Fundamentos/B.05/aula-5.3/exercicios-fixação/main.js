c1eff9aa5dd661edcc0de40478c6727c6992380f
rafaelPermec <geekchef.dev@gmail.com>
2021-11-23 14:13:58 -0300

    fixação ex. 1

---
+++ modulo.1-Fundamentos/B.05/aula-5.3/exercicios-fixação/main.js
@@ -0,0 +1,16 @@
+// 1. Copie esse arquivo e edite apenas ele;
+// 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
+// 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+
+const firstLi = document.getElementById('first-li');
+const secondLi = document.getElementById('second-li');
+const thirdLi = document.getElementById('third-li');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('my-spotrybefy');
+
+console.log(firstLi);
+/* Por causa da classe '.tech' */
+
+// 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
+// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+console.log(document.getElementsByClassName('tech'));
