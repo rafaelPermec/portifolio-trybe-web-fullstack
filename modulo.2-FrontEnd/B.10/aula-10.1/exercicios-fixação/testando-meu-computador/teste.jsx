cc902c14cba40dd24baf6224a46c3c23dbfaf499
rafaelPermec <geekchef.dev@gmail.com>
2022-01-24 17:15:50 -0300

    reset no React local :recycle:

--- modulo.2-FrontEnd/B.10/aula-10.1/exercicios-fixação/testando-meu-computador/teste.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-const textJSX = 'Hello, JSX! Je suis Rafael Perdigão.';
-const classe = 'header__catchphrase';
-const elementDiv = <h1 className={classe}>{textJSX}</h1>;
-
-const elementToStage = React.createElement(elementDiv);ReactDOM.render(elementToStage, document.getElementById('root'));
