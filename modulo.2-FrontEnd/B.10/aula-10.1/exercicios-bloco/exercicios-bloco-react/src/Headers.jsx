f33af1690b3a0685bff4c078b9a46831ee1bb379
rafaelPermec <geekchef.dev@gmail.com>
2022-01-25 16:07:35 -0300

    :tada: aula 10.2

--- modulo.2-FrontEnd/B.10/aula-10.1/exercicios-bloco/exercicios-bloco-react/src/Headers.jsx
+++ modulo.2-FrontEnd/B.10/aula-10.1/exercicios-bloco/exercicios-bloco-react/src/Headers.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component {
  render() {
    return (
-  <header>
+  <header className='header'>
      <h1>Conteúdos de Front-End</h1>
   </header>
    )
