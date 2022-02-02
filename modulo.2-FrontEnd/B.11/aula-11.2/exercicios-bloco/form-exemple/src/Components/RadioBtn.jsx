46ca9e25f263a60a7487d13e745d3fd2a48bd0f7
rafaelPermec <geekchef.dev@gmail.com>
2022-02-02 15:18:28 -0300

    :recycle: fixação-bloco

--- modulo.2-FrontEnd/B.11/aula-11.2/exercicios-bloco/form-exemple/src/Components/RadioBtn.jsx
+++ modulo.2-FrontEnd/B.11/aula-11.2/exercicios-bloco/form-exemple/src/Components/RadioBtn.jsx
@@ -5,16 +5,15 @@ class RadioBtn extends Component {
     return (
       
       <div className='radio-btn'>
-      <label htmlFor="apto-casa" required >
-        Qual a sua moradia?
-        <label className='apto-casa' htmlFor="apto"> Apartamento:
+      <label htmlFor="apto-casa" required >Qual a sua moradia?
+        <label className='apto-casa' htmlFor="apto">Apartamento:
           <input
             type="radio"
             name="apto-casa"
             id="apto"
             value="apto" />
         </label>
-        <label className='apto-casa' htmlFor="casa"> Casa:
+        <label className='apto-casa' htmlFor="casa">Casa:
             <input
               type="radio"
               name="apto-casa"
