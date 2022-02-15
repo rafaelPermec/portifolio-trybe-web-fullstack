e1605447a8f67bca9a18225282c736e51ebfe295
rafaelPermec <geekchef.dev@gmail.com>
2022-02-15 16:00:34 -0300

    :construction: fixação

--- modulo.2-FrontEnd/B.14/exercicios-fixação/testes-react/src/App.js
+++ modulo.2-FrontEnd/B.14/exercicios-fixação/testes-react/src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import React from 'react';
 import './App.css';
 
@@ -8,6 +9,8 @@ function App() {
         Email
         <input id="id-email" type="email" />
       </label>
+      <input id="btn-send" type="button" value="Enviar" />
+      <input id="btn-back" type="button" value="Voltar" />
     </div>
   );
 }
--- modulo.2-FrontEnd/B.14/exercicios-fixação/testes-react/src/App.test.js
+++ modulo.2-FrontEnd/B.14/exercicios-fixação/testes-react/src/App.test.js
@@ -1,8 +1,22 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Verificando se existe o campo Email.', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputEmail = screen.getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveProperty('type', 'email');
+});
+
+// test('Verificando se existe um botão', () => {
+//   const { getByRole } = render(<App />);
+//   const btn = getByRole('button');
+//   expect(btn).toBeInTheDocument();
+// });
+
+test('Verificando se existe dois botões', () => {
+  render(<App />);
+  const buttons = screen.getAllByRole('button');
+  expect(buttons).toHaveLength(2);
 });
