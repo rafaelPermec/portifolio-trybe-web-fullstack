0066102a9383be22e3e1fe9925117b085eb9664e
rafaelPermec <geekchef.dev@gmail.com>
2022-03-13 12:06:32 -0300

    :recycle: estudos de redux

--- modulo.2-FrontEnd/B.15/aula-15.2/exercicios-fixação/exercicio-2/missing_reducers/src/reducers/Europe.js
+++ modulo.2-FrontEnd/B.15/aula-15.2/exercicios-fixação/exercicio-2/missing_reducers/src/reducers/Europe.js
@@ -4,6 +4,13 @@ const INITIAL_STATE = {
   percentage: 0,
 };
 
-
+const europeContinent = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case EUROPE_ENERGY:
+    return { percentage: action.percentage };
+  default:
+    return state;
+  }
+};
 
 export default europeContinent;
