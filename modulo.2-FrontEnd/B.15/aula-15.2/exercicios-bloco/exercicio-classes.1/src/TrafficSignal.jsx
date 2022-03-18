554425577ec0cb3d37eb97196c7c8df278b98716
rafaelPermec <geekchef.dev@gmail.com>
2022-03-18 19:17:55 -0300

    :recycle: estudos de redux

--- modulo.2-FrontEnd/B.15/aula-15.2/exercicios-bloco/exercicio-classes.1/src/TrafficSignal.jsx
+++ modulo.2-FrontEnd/B.15/aula-15.2/exercicios-bloco/exercicio-classes.1/src/TrafficSignal.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import redSignal from './images/redSignal.jpeg';
 import greenSignal from './images/greenSignal.jpeg';
 import yellowSignal from './images/yellowSignal.jpeg';
+import { changeSignal } from './redux/actionCreators';
 
 class TrafficSignal extends React.Component {
 
@@ -25,9 +27,9 @@ class TrafficSignal extends React.Component {
     return (
       <div>
         <div className="button-container">
-          <button type="button">Red</button>
-          <button type="button">Yellow</button>
-          <button type="button">Green</button>
+          <button onClick={ () => changeSignal('red') } type="button">Red</button>
+          <button onClick={ () => changeSignal('yellow') } type="button">Yellow</button>
+          <button onClick={ () => changeSignal('green') } type="button">Green</button>
         </div>
         <img className="signal" src={this.renderSignal(signalColor)} alt="" />
       </div>
@@ -35,12 +37,12 @@ class TrafficSignal extends React.Component {
   }
 }
 
-const mapStateToProps = () => ({
-  signalColor: '' // Seu código aqui
+const mapStateToProps = (state) => ({
+  signalColor: state.signal.color,
 });
 
-const mapDispatchToProps = () => ({
-  changeSignal: '' // Seu código aqui
+const mapDispatchToProps = (dispatch) => ({
+  changeSignal: (payload) => dispatch(changeSignal(payload))
 });
 
-export default TrafficSignal;
+export default connect(mapStateToProps, mapDispatchToProps)(TrafficSignal);
