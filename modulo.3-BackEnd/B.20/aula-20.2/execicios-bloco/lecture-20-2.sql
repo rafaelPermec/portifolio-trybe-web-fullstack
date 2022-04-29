bd5dd5dc249bc48f6287eb1d1a3732e9ebee0d60
rafaelPermec <geekchef.dev@gmail.com>
2022-04-29 10:58:07 -0300

    :construction: primeiros passos em mySQL

--- modulo.3-BackEnd/B.20/aula-20.2/execicios-bloco/lecture-20-2.sql
+++ modulo.3-BackEnd/B.20/aula-20.2/execicios-bloco/lecture-20-2.sql
@@ -1,10 +1,18 @@
 -- 1. Escreva uma query para exibir a string "This is SQL Exercise, Practice and Solution".
-
+SELECT "This is SQL Exercise, Practice and Solution" AT Projects;
+SELECT "Rafael Perdigão" AT Scientist;
 -- 2. Escreva uma query para exibir três números em três colunas.
+SELECT 42 AT the_awnser;
+SELECT 77 AT like_in_cbgb;
+SELECT 86 AT soldier_down;
 -- 3. Escreva uma query para exibir a soma dos números 10 e 15.
+SELECT (10+15) AT mathematics;
 -- 4. Escreva uma query para exibir o resultado de uma expressão aritmética qualquer.
+SELECT COUNT(10+15) AT mathematics;
 -- 5. Escreva uma query para exibir todas as informações de todos os cientistas.
+SELECT * FROM Scientists;
 -- 6. Escreva uma query para exibir o nome como "Nome do Projeto" e as horas como "Tempo de Trabalho" de cada projeto.
+
 -- 7. Escreva uma query para exibir o nome dos cientistas em ordem alfabética.
 -- 8. Escreva uma query para exibir o nome dos projetos em ordem alfabética descendente.
 -- 9. Escreva uma query que exiba a string "O projeto Name precisou de Hours horas para ser concluído." para cada projeto.
