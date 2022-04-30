d12107885804fd7a2ab145d86f4f32d5e13ac202
rafaelPermec <geekchef.dev@gmail.com>
2022-04-30 14:39:52 -0300

    :construction: bonuus lecture: mySQL

---
+++ modulo.3-BackEnd/B.20/aula-20.2/exercicios-fixação/sqlcommands-lecture2.sql
@@ -0,0 +1,6 @@
+-- 1. Escreva uma query para exibir a peça e o preço de tudo que é provido pela empresa RBT .
+
+-- 2. Escreve uma query para exibir todas as informações das cinco peças com os maiores preços.
+-- 3. Escreva uma query para exibir o nome das empresas e preço das peças com os quatro maiores preços, começando a lista a partir do 3º item.
+-- 4. Escreva uma query para exibir todas as informações das peças que são providas pela empresa HAL . Ordene o resultado pelos preços das peças de forma decrescente.
+-- 5. Escreva uma query para exibir por quantas empresas a peça 1 é provida.
