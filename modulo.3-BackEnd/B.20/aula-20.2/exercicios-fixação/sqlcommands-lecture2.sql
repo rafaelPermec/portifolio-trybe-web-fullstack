bbf62f898eb14307c4c2e2e41153f1e409b77354
rafaelPermec <geekchef.dev@gmail.com>
2022-05-03 16:21:01 -0300

    :construction: bonus lecture: mySQL

--- modulo.3-BackEnd/B.20/aula-20.2/exercicios-fixação/sqlcommands-lecture2.sql
+++ modulo.3-BackEnd/B.20/aula-20.2/exercicios-fixação/sqlcommands-lecture2.sql
@@ -5,4 +5,5 @@ SELECT * FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 5;
 -- 3. Escreva uma query para exibir o nome das empresas e preço das peças com os quatro maiores preços, começando a lista a partir do 3º item.
 SELECT * FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 4 OFFSET 3;
 -- 4. Escreva uma query para exibir todas as informações das peças que são providas pela empresa HAL . Ordene o resultado pelos preços das peças de forma decrescente.
+SELECT * FROM PiecesProviders.Provides WHERE Provider = "HAL" ORDER BY Price DESC;
 -- 5. Escreva uma query para exibir por quantas empresas a peça 1 é provida.
