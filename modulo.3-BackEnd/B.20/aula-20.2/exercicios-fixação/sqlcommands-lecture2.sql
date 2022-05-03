199f3c5a9be806e19163b8b4fc926645aa24c82b
rafaelPermec <geekchef.dev@gmail.com>
2022-05-03 16:21:59 -0300

    :construction: bonus lecture: mySQL

--- modulo.3-BackEnd/B.20/aula-20.2/exercicios-fixação/sqlcommands-lecture2.sql
+++ modulo.3-BackEnd/B.20/aula-20.2/exercicios-fixação/sqlcommands-lecture2.sql
@@ -7,3 +7,4 @@ SELECT * FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 4 OFFSET 3;
 -- 4. Escreva uma query para exibir todas as informações das peças que são providas pela empresa HAL . Ordene o resultado pelos preços das peças de forma decrescente.
 SELECT * FROM PiecesProviders.Provides WHERE Provider = "HAL" ORDER BY Price DESC;
 -- 5. Escreva uma query para exibir por quantas empresas a peça 1 é provida.
+SELECT COUNT(Piece) FROM PiecesProviders.Provides WHERE Piece = 1;
