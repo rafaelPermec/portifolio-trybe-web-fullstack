04e77c3544d6e08f38249aeb126d38d704dbdc50
rafaelPermec <geekchef.dev@gmail.com>
2022-05-03 16:10:53 -0300

    :construction: bonus lecture: mySQL

--- modulo.3-BackEnd/B.20/aula-20.2/exercicios-fixação/sqlcommands-lecture2.sql
+++ modulo.3-BackEnd/B.20/aula-20.2/exercicios-fixação/sqlcommands-lecture2.sql
@@ -1,5 +1,5 @@
 -- 1. Escreva uma query para exibir a peça e o preço de tudo que é provido pela empresa RBT .
-
+SELECT * FROM PiecesProviders.Provides WHERE Provider = "RBT";
 -- 2. Escreve uma query para exibir todas as informações das cinco peças com os maiores preços.
 -- 3. Escreva uma query para exibir o nome das empresas e preço das peças com os quatro maiores preços, começando a lista a partir do 3º item.
 -- 4. Escreva uma query para exibir todas as informações das peças que são providas pela empresa HAL . Ordene o resultado pelos preços das peças de forma decrescente.
