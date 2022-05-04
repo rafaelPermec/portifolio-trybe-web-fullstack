605ee819cd3132358a5e9eb99487afb8092aaa58
rafaelPermec <geekchef.dev@gmail.com>
2022-05-04 13:02:17 -0300

    :construction: feat: mySQL filters lecture

---
+++ modulo.3-BackEnd/B.20/aula-20.3/exercicios-fixação/lecture-pecasSuppliers.sql
@@ -0,0 +1,13 @@
+-- 1. Escreva uma query para exibir todas as peças que começam com GR.
+SELECT * FROM PecasFornecedores.Pecas WHERE name LIKE 'gr%';
+-- 2. Escreva uma query para exibir todos os fornecimentos que contenham a peça com code 2. Organize o resultado por ordem alfabética de fornecedor.
+
+-- 3. Escreva uma query para exibir as peças, preço e fornecedor de todos os fornecimentos em que o código do fornecedor tenha a letra N.
+
+-- 4. Escreva uma query para exibir todas as informações dos fornecedores que são empresas limitadas (LTDA). Ordene os resultados em ordem alfabética decrescente.
+
+-- 5. Escreva uma query para exibir o número de empresas (fornecedores) que contém a letra F no código.
+
+-- 6. Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00. Ordene os resultados por ordem crescente.
+
+-- 7. Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019.
