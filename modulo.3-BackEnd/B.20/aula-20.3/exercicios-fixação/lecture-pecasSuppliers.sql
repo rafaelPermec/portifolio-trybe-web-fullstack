699304910ec2e0d37a5dac8d77ba5ac6e93abecf
rafaelPermec <geekchef.dev@gmail.com>
2022-05-04 13:15:58 -0300

    :construction: feat: mySQL filters lecture

--- modulo.3-BackEnd/B.20/aula-20.3/exercicios-fixação/lecture-pecasSuppliers.sql
+++ modulo.3-BackEnd/B.20/aula-20.3/exercicios-fixação/lecture-pecasSuppliers.sql
@@ -12,7 +12,12 @@ SELECT * FROM PecasFornecedores.Fornecedores
 WHERE name LIKE '%LTDA' 
 ORDER BY name ASC;
 -- 5. Escreva uma query para exibir o número de empresas (fornecedores) que contém a letra F no código.
-
+SELECT * FROM PecasFornecedores.Fornecedores
+WHERE code LIKE '%F%';
 -- 6. Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00. Ordene os resultados por ordem crescente.
-
+SELECT * FROM PecasFornecedores.Fornecimentos
+WHERE Preco >= 15 AND Preco <= 40
+ORDER BY Fornecedor ASC;
 -- 7. Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019.
+SELECT COUNT(*) FROM PecasFornecedores.Vendas
+WHERE order_date BETWEEN '2018-04-15' AND '2019-07-30';
