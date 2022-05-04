0882e3780f3ee4b2919b5b91b0ec740804d901ce
rafaelPermec <geekchef.dev@gmail.com>
2022-05-04 13:09:38 -0300

    :construction: feat: mySQL filters lecture

--- modulo.3-BackEnd/B.20/aula-20.3/exercicios-fixação/lecture-pecasSuppliers.sql
+++ modulo.3-BackEnd/B.20/aula-20.3/exercicios-fixação/lecture-pecasSuppliers.sql
@@ -1,11 +1,16 @@
 -- 1. Escreva uma query para exibir todas as peças que começam com GR.
-SELECT * FROM PecasFornecedores.Pecas WHERE name LIKE 'gr%';
+SELECT * FROM PecasFornecedores.Pecas 
+WHERE name LIKE 'gr%';
 -- 2. Escreva uma query para exibir todos os fornecimentos que contenham a peça com code 2. Organize o resultado por ordem alfabética de fornecedor.
-
+SELECT * FROM PecasFornecedores.Fornecimentos
+WHERE code LIKE '%2%' ORDER BY Fornecedor ASC;
 -- 3. Escreva uma query para exibir as peças, preço e fornecedor de todos os fornecimentos em que o código do fornecedor tenha a letra N.
-
+SELECT peca, Fornecedor, Preco FROM PecasFornecedores.Fornecimentos
+WHERE Fornecedor LIKE '%N%';
 -- 4. Escreva uma query para exibir todas as informações dos fornecedores que são empresas limitadas (LTDA). Ordene os resultados em ordem alfabética decrescente.
-
+SELECT * FROM PecasFornecedores.Fornecedores
+WHERE name LIKE '%LTDA' 
+ORDER BY name ASC;
 -- 5. Escreva uma query para exibir o número de empresas (fornecedores) que contém a letra F no código.
 
 -- 6. Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00. Ordene os resultados por ordem crescente.
