a67e07589ae68125dcd845e5c54bce89879c055d
rafaelPermec <geekchef.dev@gmail.com>
2022-05-05 10:52:46 -0300

    :construction: feat: manipulating SQL tables

--- modulo.3-BackEnd/B.20/aula-20.4/exercicios-fixação/command-lectures.sql
+++ modulo.3-BackEnd/B.20/aula-20.4/exercicios-fixação/command-lectures.sql
@@ -3,13 +3,18 @@
 -- Procurando Nemo, de John Lasseter, lançado em 2003, com 107 minutos de duração.
 -- Os Incríveis, de Brad Bird, lançado em 2004, com 116 minutos de duração.
 -- WALL-E, de Pete Docter, lançada em 2008, com 104 minutos de duração.
+
 INSERT INTO Movies(title, director, year, length_minutes)
 VALUES 
   ('Monstros SA', 'Peter Docter', 2001, 92),
   ('Procurando Nemo', 'John Lasseter', 2003, 107),
   ('Os Incríveis', 'Brad Bird', 2004, 116),
   ('WALL-E', 'Pete Docter', 2008, 104);
+
 -- Exercício 2: Procurando Nemo foi aclamado pela crítica! Foi classificado em 6.8, fez 450 milhões no mercado interno e 370 milhões no mercado internacional. Adicione as informações à tabela BoxOffice.
+
+INSERT INTO BoxOffice(movie_id, rating, domestic_sales, international_sales)
+VALUES (9, 6.8, 450000000, 370000000);
 
 -- Exercício 3: O diretor do filme "Procurando Nemo" está incorreto, na verdade ele foi dirigido por Andrew Staton. Corrija esse dado utilizando o UPDATE.
 
