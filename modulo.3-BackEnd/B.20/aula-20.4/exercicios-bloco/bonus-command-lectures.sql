1623bd60860b2bb055abda3ad1511ccbe3750eef
rafaelPermec <geekchef.dev@gmail.com>
2022-05-05 12:33:30 -0300

    :construction: feat: manipulating SQL tables - bonus lecture

--- modulo.3-BackEnd/B.20/aula-20.4/exercicios-bloco/bonus-command-lectures.sql
+++ modulo.3-BackEnd/B.20/aula-20.4/exercicios-bloco/bonus-command-lectures.sql
@@ -2,6 +2,10 @@
 
 -- Exercício 8: Altere a classificação da tabela BoxOffice para 9.0 de todos os filmes que lucraram mais de 400 milhões no mercado interno.
 
+UPDATE Pixar.BoxOffice
+SET rating = 9.0
+WHERE domestic_sales > 400000000;
+
 -- Exercício 9: Altere a classificação da tabela BoxOffice para 6.0 de todos os filmes que lucraram menos de 300 milhões no mercado internacional e mais de 200 milhões no mercado interno.
 
 -- Exercício 10: Exclua da tabela Movies todos os filmes com menos de 100 minutos de duração.
