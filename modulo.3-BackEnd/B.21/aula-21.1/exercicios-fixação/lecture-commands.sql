3118dd2453234a9f0537d1dc7fa567622e142283
rafaelPermec <geekchef.dev@gmail.com>
2022-05-06 17:05:24 -0300

    :construction: lecture: mySQL functions

--- modulo.3-BackEnd/B.21/aula-21.1/exercicios-fixação/lecture-commands.sql
+++ modulo.3-BackEnd/B.21/aula-21.1/exercicios-fixação/lecture-commands.sql
@@ -1,15 +1,26 @@
 -- 1. Escreva uma query que exiba o maior salário da tabela.
-
+SELECT MAX(salary) FROM hr.employees;
 -- 2. 🚀 Escreva uma query que exiba a diferença entre o maior e o menor salário.
-
+SELECT MAX(salary) - MIN(salary) FROM hr.employees;
 -- 3. 🚀 Escreva uma query que exiba a média salarial de cada JOB_ID, ordenando pela média salarial em ordem decrescente.
-
+SELECT JOB_ID, ROUND(AVG(salary), 2) AS salario_medio
+FROM hr.employees
+GROUP BY JOB_ID
+ORDER BY salario_medio DESC;
 -- 4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias.
-
+SELECT SUM(salary) AS pagamento
+FROM hr.employees;
 -- 5. 🚀 Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
-
+SELECT ROUND(MAX(salary), 2) AS maior_salario, 
+ROUND(MIN(salary), 2) AS menor_salario, 
+ROUND(SUM(salary), 2) AS folha_de_pagamento,
+ROUND(AVG(salary), 2) AS media_salarial 
+FROM hr.employees;
 -- 6. Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras (IT_PROG).
-
+SELECT JOB_ID, COUNT(*) AS `pessoas programadoras`
+FROM hr.employees
+GROUP BY JOB_ID
+HAVING JOB_ID = 'IT_PROG';
 -- 7. Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão (JOB_ID).
 
 -- 8. Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras (IT_PROG).
