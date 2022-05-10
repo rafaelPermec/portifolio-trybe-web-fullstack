5c52f8c835986e92412844e85503f2d05812a083
rafaelPermec <geekchef.dev@gmail.com>
2022-05-10 15:22:49 -0300

    :construction: feat: mySQL JOIN functions exercise

--- modulo.3-BackEnd/B.21/aula-21.2/exercicios-bloco/new-pixar-commands.sql
+++ modulo.3-BackEnd/B.21/aula-21.2/exercicios-bloco/new-pixar-commands.sql
@@ -22,3 +22,8 @@ LEFT JOIN `New-Pixar`.Theater AS t
 ON t.id = m.theater_id
 ORDER BY t.name;
 -- Exercício 5: Utilizando o RIGHT JOIN, faça uma busca que retorne todos os dados dos filmes, mesmo os que não estão em cartaz e, adicionalmente, os dados dos cinemas que possuem estes filmes em cartaz. Retorne os nomes dos cinemas em ordem alfabética.
+SELECT m.title, m.director, m.length_minutes, t.name, t.location
+FROM `New-Pixar`.Theater AS t
+RIGHT JOIN  `New-Pixar`.Movies AS m
+ON t.id = m.theater_id
+ORDER BY t.name ASC;
