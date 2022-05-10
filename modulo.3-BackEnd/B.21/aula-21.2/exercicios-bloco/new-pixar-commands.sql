abe30ab87739888542c309d197e264057af777d9
rafaelPermec <geekchef.dev@gmail.com>
2022-05-10 14:45:50 -0300

    :construction: feat: mySQL JOIN functions exercise

--- modulo.3-BackEnd/B.21/aula-21.2/exercicios-bloco/new-pixar-commands.sql
+++ modulo.3-BackEnd/B.21/aula-21.2/exercicios-bloco/new-pixar-commands.sql
@@ -4,9 +4,17 @@ FROM `New-Pixar`.Movies AS m
 INNER JOIN `New-Pixar`.BoxOffice AS b 
 ON b.movie_id = m.id;
 -- 🚀 Exercício 2: Utilizando o INNER JOIN, faça uma busca que retorne o número de vendas para cada filme que possui um número maior de vendas internacionais (international_sales) do que vendas nacionais (domestic_sales).
-
+SELECT m.title, (b.domestic_sales + b.international_sales) AS `Vendas`
+FROM `New-Pixar`.Movies AS m
+INNER JOIN `New-Pixar`.BoxOffice AS b 
+ON b.movie_id = m.id
+WHERE b.domestic_sales < b.international_sales;
 -- 🚀 Exercício 3: Utilizando o INNER JOIN, faça uma busca que retorne os filmes e sua avaliação (rating) em ordem decrescente.
-
+SELECT m.title, b.rating AS `Notas`
+FROM `New-Pixar`.Movies AS m
+INNER JOIN `New-Pixar`.BoxOffice AS b 
+ON b.movie_id = m.id
+ORDER BY b.rating DESC;
 -- Exercício 4: Utilizando o LEFT JOIN, faça uma busca que retorne todos os dados dos cinemas, mesmo os que não possuem filmes em cartaz e, adicionalmente, os dados dos filmes que estão em cartaz nestes cinemas. Retorne os nomes dos cinemas em ordem alfabética.
 
 -- Exercício 5: Utilizando o RIGHT JOIN, faça uma busca que retorne todos os dados dos filmes, mesmo os que não estão em cartaz e, adicionalmente, os dados dos cinemas que possuem estes filmes em cartaz. Retorne os nomes dos cinemas em ordem alfabética.
