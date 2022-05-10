164366b8a400c0cefba1513e0bada010f7391e0c
rafaelPermec <geekchef.dev@gmail.com>
2022-05-10 14:41:42 -0300

    :construction: feat: mySQL JOIN functions exercise

---
+++ modulo.3-BackEnd/B.21/aula-21.2/exercicios-bloco/new-pixar-commands.sql
@@ -0,0 +1,12 @@
+-- Exercício 1: Utilizando o INNER JOIN, encontre as vendas nacionais (domestic_sales) e internacionais (international_sales) de cada filme.
+SELECT m.title, b.domestic_sales, b.international_sales
+FROM `New-Pixar`.Movies AS m
+INNER JOIN `New-Pixar`.BoxOffice AS b 
+ON b.movie_id = m.id;
+-- 🚀 Exercício 2: Utilizando o INNER JOIN, faça uma busca que retorne o número de vendas para cada filme que possui um número maior de vendas internacionais (international_sales) do que vendas nacionais (domestic_sales).
+
+-- 🚀 Exercício 3: Utilizando o INNER JOIN, faça uma busca que retorne os filmes e sua avaliação (rating) em ordem decrescente.
+
+-- Exercício 4: Utilizando o LEFT JOIN, faça uma busca que retorne todos os dados dos cinemas, mesmo os que não possuem filmes em cartaz e, adicionalmente, os dados dos filmes que estão em cartaz nestes cinemas. Retorne os nomes dos cinemas em ordem alfabética.
+
+-- Exercício 5: Utilizando o RIGHT JOIN, faça uma busca que retorne todos os dados dos filmes, mesmo os que não estão em cartaz e, adicionalmente, os dados dos cinemas que possuem estes filmes em cartaz. Retorne os nomes dos cinemas em ordem alfabética.
