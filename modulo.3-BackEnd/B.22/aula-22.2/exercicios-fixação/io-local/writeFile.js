db5d4455fa4cd4f1967873139761131489628f99
rafaelPermec <geekchef.dev@gmail.com>
2022-05-16 19:10:06 -0300

    :construction: feat: fs.writeFile lecture

--- modulo.3-BackEnd/B.22/aula-22.2/exercicios-fixação/io-local/writeFile.js
+++ modulo.3-BackEnd/B.22/aula-22.2/exercicios-fixação/io-local/writeFile.js
@@ -3,5 +3,5 @@ const fs = require('fs/promises');
 const fileName = 'my-file.txt';
 
 fs.writeFile(fileName, 'My textbook: right here. Right Now.')
-  .then(() => console.log(`File writen with success!`))
+  .then(() => console.log(`File written with success!`))
   .catch((err) => console.error(`Cannot wrote in that file:\n ${err.message}`));
