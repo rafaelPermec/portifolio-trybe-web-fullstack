6c717ec5e1318d4e3b2f4c49e367e2a102ced3d2
rafaelPermec <geekchef.dev@gmail.com>
2022-05-18 16:46:07 -0300

    :construction: feat: node.js + express/nodemon exercise

---
+++ modulo.3-BackEnd/B.22/aula-22.4/exercicios-fixação/drinks-api/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const drinks = [
+	{ id: 1, name: 'Refrigerante Lata', price: 5.0 },
+	{ id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+	{ id: 3, name: 'Suco 300ml', price: 4.0 },
+	{ id: 4, name: 'Suco 1l', price: 10.0 },
+	{ id: 5, name: 'Cerveja Lata', price: 4.5 },
+	{ id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
+app.get('/drinks', (_req, res) => {
+  const sortedDrinks = drinks.sort((a, b) => a.name < b.name ? 1 : -1)
+  res.json(sortedDrinks);
+});
+
+app.listen(3001, () => {
+  console.log('App is listening to port 3001');
+});
