204b0d2a6f827e99d8b2f6e306af221eb5c0dbf7
rafaelPermec <geekchef.dev@gmail.com>
2022-06-05 01:57:52 -0300

    :construction: feat: node.js / MSC - Models Exercises

--- modulo.3-BackEnd/B.23/aula-23.1/exercicios-bloco/user-CRUD-db/models/Users.js
+++ modulo.3-BackEnd/B.23/aula-23.1/exercicios-bloco/user-CRUD-db/models/Users.js
@@ -15,26 +15,20 @@ const serialize = ({ id, first_name: firstName, last_name: lastName, email }) =>
 const createUser = async ({ firstName, lastName, email, password }) => {
   const query = 'INSERT INTO `users_crud`.users (first_name, last_name, email, password) VALUES (?,?,?,?);';
 
-  const executeQuery = await connection.execute(query, [firstName, lastName, email, password])
+  await connection.execute(query, [firstName, lastName, email, password])
     .then(([result]) => ({ id: result.insertId, firstName, lastName, email }));
-
-  return executeQuery;
 };
 
 const getUser = async () => {
   const query = 'SELECT * FROM `users_crud`.users;'
-  const executeQuery = await connection.execute(query)
+  await connection.execute(query)
     .then(([results]) => results.map(serialize));
-
-  return executeQuery;
 };
 
 const getById = async (id) => {
   const query = 'SELECT * FROM `users_crud`.users WHERE id = ?;';
-  const executeQuery = await connection.execute(query, [id])
+  await connection.execute(query, [id])
     .then(([results]) => (!results[0] ? null : serialize(results[0])));
-
-  return executeQuery;
 }
 
 const updateUser = async (id, { firstName, lastName, email, password }) => {
