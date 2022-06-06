059f551ca61e94c4e9a1b77aed0679c990a38af4
rafaelPermec <geekchef.dev@gmail.com>
2022-06-06 12:10:04 -0300

    :construction: feat: node.js / MSC - Service & Controller Exercises

---
+++ modulo.3-BackEnd/B.23/aula-23.2/exercicios-fixação/hello-msc/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const Author = require('./Services/Authors');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/authors', async (_req, res) => {
+  const authors = await Author.getAll();
+
+  res.status(200).json(authors);
+});
+
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({ message: 'Not found' });
+
+  res.status(200).json(author);
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  const author = await Author.createAuthor(first_name, middle_name, last_name);
+
+  if (!author) return res.status(400).json({ message: 'Dados inválidos' });
+
+  res.status(201).json(author);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
+});
