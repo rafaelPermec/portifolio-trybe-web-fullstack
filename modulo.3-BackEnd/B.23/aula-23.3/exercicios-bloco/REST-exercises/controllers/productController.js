4a3c57e84f00d74f6e6e1ca9373d541effa7b784
rafaelPermec <geekchef.dev@gmail.com>
2022-06-08 15:48:31 -0300

    :construction: feat: node.js / node-fetch / REST & RESTful exercises

--- modulo.3-BackEnd/B.23/aula-23.3/exercicios-bloco/REST-exercises/controllers/productController.js
+++ modulo.3-BackEnd/B.23/aula-23.3/exercicios-bloco/REST-exercises/controllers/productController.js
@@ -3,33 +3,43 @@ const ProductModel = require('../models/productModel');
 
 const router = express.Router();
 
-router.get('/list-products', async (req, res) => {
+router.get('/', async (_req, res) => {
   const products = await ProductModel.getAll();
 
   res.status(200).json(products);
 });
 
-router.get('/get-by-id/:id', async (req, res) => {
-  const product = await ProductModel.getById(req.params.id);
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await ProductModel.getById(id);
+
+  if (!product) return res.status(404).json({ message: 'Produto não encontrado' });
 
   res.status(200).json(product);
 });
 
-router.post('/add-product', async (req, res) => {
+router.post('/', async (req, res) => {
   const { name, brand } = req.body;
+
+  if (!name || !brand) return res.status(400).json({ message: 'Informações do produto inválidas' });
 
   const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+  res.status(201).json(newProduct);
 });
 
-router.post('/delete-product/:id', async (req, res) => {
-  const products = await ProductModel.exclude(req.params.id);
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await ProductModel.getById(id);
 
-  res.send(products);
+  if (!product) return res.status(404).json({ message: 'Produto não encontrado' });
+
+  await ProductModel.exclude(id);
+
+  res.status(204).json({ message: 'Produto removido.' });
 });
 
-router.post('/update-product/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
 
   const products = await ProductModel.update(req.params.id, name, brand);
