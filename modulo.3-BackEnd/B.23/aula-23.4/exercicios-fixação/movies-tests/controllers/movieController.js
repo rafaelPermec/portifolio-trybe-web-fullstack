f49f76287b1f99ab45d9e7e9b46366ac05fdfbce
rafaelPermec <geekchef.dev@gmail.com>
2022-06-16 18:29:12 -0300

    :construction: feat: testes mocha, chai e sinon em REST API's com base no modelo de arquitetura MSC

---
+++ modulo.3-BackEnd/B.23/aula-23.4/exercicios-fixação/movies-tests/controllers/movieController.js
@@ -0,0 +1,15 @@
+const MoviesService = require('../services/movieService');
+
+const create = async (req, res) => {
+  const { title, directedBy, releaseYear } = req.body;
+
+  const movie = await MoviesService.create({ title, directedBy, releaseYear });
+
+  if (!movie) return res.status(400).send('Dados Inválidos');
+
+  res.status(201).send('Filme criado com sucesso!');
+};
+
+module.exports = {
+  create,
+};
