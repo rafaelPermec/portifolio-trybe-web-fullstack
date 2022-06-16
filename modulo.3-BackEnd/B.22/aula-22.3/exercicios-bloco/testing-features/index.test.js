2e8ea7b6a06b712d0a873f710dcf8031364228c4
rafaelPermec <geekchef.dev@gmail.com>
2022-06-15 21:18:27 -0300

    :construction: feat: primeiros passos com testes mocha e chai no node.js

--- modulo.3-BackEnd/B.22/aula-22.3/exercicios-bloco/testing-features/index.test.js
+++ modulo.3-BackEnd/B.22/aula-22.3/exercicios-bloco/testing-features/index.test.js
@@ -3,4 +3,24 @@ const sinon = require('sinon');
 
 const { verificaNumero } = require('./index.js');
 
+describe('Valida se na aplicação.', () => {
+  describe('a função "verificaNumero":', () => {
+    it('Vai receber um número como parâmetro e retornar uma string como resposta,', () => {
 
+    });
+  });
+
+  describe('Quando o número passado for:', () => {
+    it('maior que 0 - deverá retornar "positivo":', () => {
+
+    });
+
+    it('menor que 0 - deverá retornar "negativo"', () => {
+
+    });
+
+    it('igual a 0 - deverá retornar "neutro"', () => {
+
+    });
+  });
+});
