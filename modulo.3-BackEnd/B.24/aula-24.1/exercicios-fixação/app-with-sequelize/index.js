b41f41bc7ff5f142c4cf6e3641d9eca1a49c146d
rafaelPermec <geekchef.dev@gmail.com>
2022-06-21 19:00:01 -0300

    :construction: feat: first studies about ORM patterns in node.js with Sequelize

--- modulo.3-BackEnd/B.24/aula-24.1/exercicios-fixação/app-with-sequelize/index.js
+++ modulo.3-BackEnd/B.24/aula-24.1/exercicios-fixação/app-with-sequelize/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./src/routes/userRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
