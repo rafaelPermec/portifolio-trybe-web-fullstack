3d9c7a4a383a4b0a976f3d110649466901442051
rafaelPermec <geekchef.dev@gmail.com>
2022-06-21 16:46:55 -0300

    :construction: feat: first studies about ORM patterns in node.js with Sequelize

---
+++ modulo.3-BackEnd/B.24/aula-24.1/exercicios-fixação/app-with-sequelize/models/user.js
@@ -0,0 +1,12 @@
+'use strict';
+const User = (sequelize, DataTypes) => {
+
+  const User = sequelize.define('User', {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+  });
+
+  return User;
+};
+
+module.exports = User;
