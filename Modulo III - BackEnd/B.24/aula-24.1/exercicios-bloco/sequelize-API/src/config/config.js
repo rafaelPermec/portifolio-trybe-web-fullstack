2fe83b4d3b71fc63a9a8fd43a9efac3960f9949a
rafaelPermec <geekchef.dev@gmail.com>
2022-06-22 18:27:06 -0300

    :construction: feat: first studies about ORM patterns in node.js with Sequelize

--- Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/src/config/config.js
+++ Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/src/config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.HOSTNAME,
-    dialect: "mysql"
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   },
   "test": {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.HOSTNAME,
-    dialect: "mysql"
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   },
   "production": {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.HOSTNAME,
-    dialect: "mysql"
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   }
 }
