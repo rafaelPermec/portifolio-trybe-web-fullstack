2d6e61a2ea26e0f15403314c4e3d350313766e0a
rafaelPermec <geekchef.dev@gmail.com>
2022-06-23 00:18:00 -0300

    :construction: feat: first studies about ORM patterns in node.js with Sequelize

--- Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/index.js
+++ Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const booksRoutes = require('./src/routes/bookRoutes');
+
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/books', booksRoutes);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
