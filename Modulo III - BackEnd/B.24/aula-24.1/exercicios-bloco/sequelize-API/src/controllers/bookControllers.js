e67557d8a997970b2cc20a021c4a078321927f90
rafaelPermec <geekchef.dev@gmail.com>
2022-06-26 19:43:31 -0300

    :construction: feat: first studies about ORM patterns in node.js with Sequelize

--- Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/src/controllers/bookControllers.js
+++ Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/src/controllers/bookControllers.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const BookServices = require('../services/bookServices');
 
+const getAllBooks = async (_req, res) => {
+  const books = await BookServices.getAllBooks();
+  res.status(200).json(books);
+};
+
+const getAllBooksbyId = async (id) => { };
+
 const createBooks = async (req, res) => {
   const data = req.body;
-  const newBook = await BookServices.createBooks(data)
+  const newBook = await BookServices.createBooks(data);
   return res.status(201).json(newBook);
+};
+
+const postBooks = async () => { };
+
+const deleteBooks = async () => { };
+
+
+module.exports = {
+  getAllBooks,
+  getAllBooksbyId,
+  createBooks,
+  postBooks,
+  deleteBooks,
 };
--- Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/src/services/bookServices.js
+++ Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/src/services/bookServices.js
@@ -1,1 +1,22 @@
 const { Books } = require('../models');
+
+const getAllBooks = async () => {
+  const books = await Books.findAll();
+  return books;
+};
+
+const getAllBooksbyId = async (id) => { };
+
+const createBooks = async () => { };
+
+const postBooks = async () => { };
+
+const deleteBooks = async () => { };
+
+module.exports = {
+  getAllBooks,
+  getAllBooksbyId,
+  createBooks,
+  postBooks,
+  deleteBooks,
+};
