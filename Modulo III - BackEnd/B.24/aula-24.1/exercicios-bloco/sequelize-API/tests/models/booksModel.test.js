d10e91e44e38b1ee69718a94b52462730ca8b579
rafaelPermec <geekchef.dev@gmail.com>
2022-06-27 09:20:09 -0300

    :construction: feat: first studies about ORM patterns in node.js with Sequelize

--- Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/tests/models/booksModel.test.js
+++ Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/tests/models/booksModel.test.js
@@ -1,5 +1,12 @@
 const { expect } = require('chai');
 const { Book } = require('../../src/models');
+const BookModel = require('../../src/models/book');
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkPropertyExists
+} = require('sequelize-test-helpers');
 
 describe('O model de Book', function () {
   it('possui a propriedade "title"', function () {
@@ -25,5 +32,18 @@ describe('O model de Book', function () {
   it('possui a propriedade "publisher"', function () {
     const book = new Book();
     expect(book).to.have.property('publisher');
+  });
+});
+
+describe('O model de Book', () => {
+  const Book = BookModel(sequelize, dataTypes);
+  const book = new Book();
+
+  describe('possui o nome "Book"', () => {
+    checkModelName(Book)('Book');
+  });
+
+  describe('possui as propriedades "title", "author", "pageQuantity", "available" e "publisher"', () => {
+    ['title', 'author', 'pageQuantity', 'available', 'publisher'].forEach(checkPropertyExists(book));
   });
 });
