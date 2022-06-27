6ae8d7940f9d48d8057c2f232a795abb19c2e957
rafaelPermec <geekchef.dev@gmail.com>
2022-06-27 09:01:25 -0300

    :construction: feat: first studies about ORM patterns in node.js with Sequelize

--- Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/src/models/book.js
+++ Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/src/models/book.js
@@ -2,7 +2,9 @@ const Bookschema = (sequelize, DataTypes) => {
   const BooksTable = sequelize.define("Book", {
     title: DataTypes.STRING,
     author: DataTypes.STRING,
-    pageQuantity: DataTypes.INTEGER
+    pageQuantity: DataTypes.INTEGER,
+    available: DataTypes.BOOLEAN,
+    publisher: DataTypes.STRING,
   });
 
   return BooksTable;
--- Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/src/services/bookServices.js
+++ Modulo III - BackEnd/B.24/aula-24.1/exercicios-bloco/sequelize-API/src/services/bookServices.js
@@ -10,20 +10,25 @@ const getAllBooksbyId = async (id) => {
   return book;
 };
 
-const createBooks = async ({ title, author, pageQuantity, available }) => {
-  const newBook = Books.create({ title, author, pageQuantity, available });
+const createBooks = async ({ title, author, pageQuantity, available, publisher }) => {
+  const newBook = Books.create({ title, author, pageQuantity, available, publisher });
   return newBook;
 };
 
 const updateBooks = async (id, { title, author, pageQuantity, available }) => {
   const [updated] = await Books
-    .update({ title, author, pageQuantity, available }, { where: { id } });
+    .update({ title, author, pageQuantity, available, publisher }, { where: { id } });
   return updated;
 };
 
 const deleteBooks = async (id) => {
   const removedBook = await Books.destroy({ where: { id } });
   return removedBook;
+};
+
+const getByAuthor = async (author) => {
+  const bookByAuthor = await Books.findAll({ where: { author } });
+  return bookByAuthor;
 };
 
 module.exports = {
