686f32d92134a5dfa55ca725efa6c90afde1902f
rafaelPermec <geekchef.dev@gmail.com>
2022-06-30 16:58:42 -0300

    :construction: feat: studing JsonWebToken process for Web authentications

---
+++ Modulo III - BackEnd/B.24/aula-24.3/exercicios-fixação/nodejs-jwt-base-project/api/auth/validateJWT.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { User } = require('../../models');
+
+const secret = process.env.JWT_SECRET;
+
+module.exports = async (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token não encontrado' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secret);
+    const user = await User.findOne({
+      where: { username: decoded.data.username }
+    });
+    if (!user) {
+      return res.status(401).json({ message: 'Erro ao procurar usuário do token.' });
+    }
+  } catch (error) {
+
+  }
+};
