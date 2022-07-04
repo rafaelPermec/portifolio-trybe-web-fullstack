ebd47af5394428002cdba3f38155aa271371f8a2
rafaelPermec <geekchef.dev@gmail.com>
2022-07-04 16:18:08 -0300

    :construction: feat: primeiros passos com TypeScript

---
+++ Modulo III - BackEnd/B.26/aula-25.1/exercicios-fixação/my-first-typescript/diasDaSemana.ts
@@ -0,0 +1,22 @@
+enum diasDaSemana {
+  domingo = 'domingo',
+  segunda = 'segunda-feira',
+  terça = 'terça-feira',
+  quarta = 'quarta-feira',
+  quinta = 'quinta-feira',
+  sexta = 'sexta-feira',
+  sabado = 'sábado',
+};
+
+const hoje = diasDaSemana.segunda;
+
+const garfield = `Eu odeio ${diasDaSemana.segunda}`;
+
+const fds = `${diasDaSemana.sabado} e ${diasDaSemana.domingo}`
+
+
+console.log(hoje);
+
+console.log(garfield);
+
+console.log(fds);
