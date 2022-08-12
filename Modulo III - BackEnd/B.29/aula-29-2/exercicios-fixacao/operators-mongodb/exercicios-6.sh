78f77212a9beecbeb97c1307a21d659aa969de32
rafaelPermec <geekchef.dev@gmail.com>
2022-08-12 19:47:31 -0300

    :construction: feat: estudos sobre operadores e filtros logicos no mongodb

---
+++ Modulo III - BackEnd/B.29/aula-29-2/exercicios-fixacao/operators-mongodb/exercicios-6.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mongo
+
+
+mongoimport --db=rafaelPermec --collection=restaurants --file=restaurants.json
+
+# Exercício 6: Selecione e faça a contagem dos restaurantes que não possuem avaliação menor ou igual a 5, essa consulta também deve retornar restaurantes que não possuem o campo de avaliação.
+
+db.restaurants.countDocuments({ rating: { $nin: [ 5, 6, 7 ] } });
