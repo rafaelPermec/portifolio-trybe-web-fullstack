ac6c8d991ffe4edd5ad8fe0517ca3b931cd015ab
rafaelPermec <geekchef.dev@gmail.com>
2022-08-14 18:47:30 -0300

    :construction: feat: intro - mongodb

---
+++ Modulo III - BackEnd/B.29/aula-29-2/exercicios-bloco/superheroes-mongodb/exercicios-19.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mongo
+
+mongoimport --db=rafaelPermec --collection=restaurants --file=../superheroes-mongodb/superheroes.json
+
+# Exercício 19: Remova todos os documentos publicados pelo George Lucas.
+
+db.superheroes.remove({ "publisher": "George Lucas" });
+
+db.superheroes.countDocuments({});
+# 733
