38ada04823d56f78c0de81f86a0dbb7a28c49742
rafaelPermec <geekchef.dev@gmail.com>
2022-08-20 23:52:22 -0300

    :construction: feat: primeiros passos com MongoDB + Mongoose

---
+++ Modulo III - BackEnd/B.30/aula-30.01/exercicios-fixacao/guia-mongodb-com-nodejs-e-poo/src/tests/unit/models/frame.test.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import FrameModel from '../../../models/Frame';
+import { Model } from 'mongoose';
+import { frameMock, frameMockWithId } from '../../mocks/frameMock';
+
+describe('Frame Model', () => {
+  const frameModel = new FrameModel();
+
+  before(() => {
+    sinon.stub(Model, 'create').resolves(frameMockWithId);
+    sinon.stub(Model, 'findOne').resolves(frameMockWithId);
+  });
+
+  after(() => {
+    sinon.restore();
+  });
+
+});
