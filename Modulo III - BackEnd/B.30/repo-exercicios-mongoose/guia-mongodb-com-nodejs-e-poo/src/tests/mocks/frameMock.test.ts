df01bd550f277fa6ac1c77becc4a258d6c4435ff
rafaelPermec <geekchef.dev@gmail.com>
2022-08-21 18:03:45 -0300

    :construction: feat: primeiros passos com MongoDB + Mongoose : models do projeto + testes em TDD prontos

--- Modulo III - BackEnd/B.30/repo-exercicios-mongoose/guia-mongodb-com-nodejs-e-poo/src/tests/mocks/frameMock.test.ts
+++ Modulo III - BackEnd/B.30/repo-exercicios-mongoose/guia-mongodb-com-nodejs-e-poo/src/tests/mocks/frameMock.test.ts
@@ -11,4 +11,17 @@ const frameMockWithId: IFrame & { _id: string } = {
   color: 'Daquele naipe'
 };
 
-export { frameMock, frameMockWithId };
+const allFrameMockWithId: IFrame[] & { _id: string }[] = [
+  {
+    _id: '62cf1fc6498565d94eba52cd',
+    material: 'Ouro',
+    color: 'Daquele naipe'
+  },
+  {
+    _id: '62cf1fc6498565d94eba52cd',
+    material: 'Prata',
+    color: 'Quase Daquele naipe'
+  },
+];
+
+export { frameMock, frameMockWithId, allFrameMockWithId };
--- Modulo III - BackEnd/B.30/repo-exercicios-mongoose/guia-mongodb-com-nodejs-e-poo/src/tests/unit/models/frame.test.ts
+++ Modulo III - BackEnd/B.30/repo-exercicios-mongoose/guia-mongodb-com-nodejs-e-poo/src/tests/unit/models/frame.test.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import FrameModel from '../../../models/frame.model';
 import { Model } from 'mongoose';
-import { frameMock, frameMockWithId } from '../../mocks/frameMock.test';
+import { allFrameMockWithId, frameMock, frameMockWithId } from '../../mocks/frameMock.test';
 
 describe('Frame Model', () => {
   const frameModel = new FrameModel();
@@ -10,6 +10,8 @@ describe('Frame Model', () => {
   before(() => {
     sinon.stub(Model, 'create').resolves(frameMockWithId);
     sinon.stub(Model, 'findOne').resolves(frameMockWithId);
+    sinon.stub(Model, 'findOneAndDelete').resolves(frameMockWithId);
+    sinon.stub(Model, 'find').resolves(allFrameMockWithId);
   });
 
   after(() => {
@@ -23,10 +25,32 @@ describe('Frame Model', () => {
     });
   });
 
+  describe('searching for frames', () => {
+    it('successfully found', async () => {
+      const frameFound = await frameModel.read();
+      expect(frameFound).to.be.deep.equal(allFrameMockWithId);
+    });
+  });
+
   describe('searching a frame', () => {
     it('successfully found', async () => {
       const framesFound = await frameModel.readOne('62cf1fc6498565d94eba52cd');
       expect(framesFound).to.be.deep.equal(frameMockWithId);
+    });
+
+    it('_id not found', async () => {
+      try {
+        await frameModel.readOne('123ERRADO');
+      } catch (error: any) {
+        expect(error.message).to.be.eq('InvalidMongoId');
+      }
+    });
+  });
+
+  describe('deleting a frame by id', () => {
+    it('successfully found', async () => {
+      const frameDeleted = await frameModel.destroy('62cf1fc6498565d94eba52cd');
+      expect(frameDeleted).to.be.deep.equal(frameMockWithId);
     });
 
     it('_id not found', async () => {
