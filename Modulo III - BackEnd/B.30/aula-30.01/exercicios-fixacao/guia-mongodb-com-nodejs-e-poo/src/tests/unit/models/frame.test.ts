03c8f2f9cf044f9ad8f877c037126a357ac1104a
rafaelPermec <geekchef.dev@gmail.com>
2022-08-20 23:53:05 -0300

    :construction: feat: primeiros passos com MongoDB + Mongoose

--- Modulo III - BackEnd/B.30/aula-30.01/exercicios-fixacao/guia-mongodb-com-nodejs-e-poo/src/tests/unit/models/frame.test.ts
+++ Modulo III - BackEnd/B.30/aula-30.01/exercicios-fixacao/guia-mongodb-com-nodejs-e-poo/src/tests/unit/models/frame.test.ts
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-import FrameModel from '../../../models/Frame';
+import FrameModel from '../../../models/frame.model';
 import { Model } from 'mongoose';
-import { frameMock, frameMockWithId } from '../../mocks/frameMock';
+import { frameMock, frameMockWithId } from '../../mocks/frameMock.test';
 
 describe('Frame Model', () => {
   const frameModel = new FrameModel();
@@ -14,6 +14,28 @@ describe('Frame Model', () => {
 
   after(() => {
     sinon.restore();
+  });
+
+  describe('creating a frame', () => {
+    it('successfully created', async () => {
+      const newFrame = await frameModel.create(frameMock);
+      expect(newFrame).to.be.deep.equal(frameMockWithId);
+    });
+  });
+
+  describe('searching a frame', () => {
+    it('successfully found', async () => {
+      const framesFound = await frameModel.readOne('62cf1fc6498565d94eba52cd');
+      expect(framesFound).to.be.deep.equal(frameMockWithId);
+    });
+
+    it('_id not found', async () => {
+      try {
+        await frameModel.readOne('123ERRADO');
+      } catch (error: any) {
+        expect(error.message).to.be.eq('InvalidMongoId');
+      }
+    });
   });
 
 });
