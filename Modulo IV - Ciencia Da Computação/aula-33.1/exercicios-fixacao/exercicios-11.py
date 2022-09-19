ce9ba62e819f4ea7d5d33da0107252fa7017fe33
rafaelPermec <geekchef.dev@gmail.com>
2022-09-19 12:34:01 -0300

    :construction: init (py): Primeiros passos em Python

--- Modulo IV - Ciencia Da Computação/aula-33.1/exercicios-fixacao/exercicios-11.py
+++ Modulo IV - Ciencia Da Computação/aula-33.1/exercicios-fixacao/exercicios-11.py
@@ -1,9 +1,10 @@
 # Exercício 11: Realizar a contagem de quantas vezes cada elemento aparece em uma sequência é uma técnica muito útil na solução de alguns problemas. Qual é a estrutura mais recomendada para o armazenamento desta contagem?
 
+from typing import Counter
+
+
 my_list = [20, 20, 1, 2]
 
-count_elements = {
-    20: 2,
-    1: 1,
-    2: 2
-}
+counter = Counter(my_list)
+
+print(counter)
