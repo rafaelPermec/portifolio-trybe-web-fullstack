48b85a7878d5e37520753a089eac7167777f1f5d
rafaelPermec <geekchef.dev@gmail.com>
2022-09-19 10:45:44 -0300

    :construction: init (py): Primeiros passos em Python

---
+++ Modulo IV - Ciencia Da Computação/aula-33.1/exercicios-fixacao/exercicios-10.py
@@ -0,0 +1,5 @@
+# Exercício 10: Após uma consulta do banco de dados, temos linhas que contém nome, sobrenome e idade como: "Thiago", "Nobre", 29. Que estrutura vista anteriormente seria recomendada dado que após esta consulta somente exibimos estes valores.
+
+db_list = ("Thiago", "Nobre", 29)
+
+print('O melhor caminho seria a tuple:', db_list)
---
+++ Modulo IV - Ciencia Da Computação/aula-33.1/exercicios-fixacao/exercicios-11.py
@@ -0,0 +1,9 @@
+# Exercício 11: Realizar a contagem de quantas vezes cada elemento aparece em uma sequência é uma técnica muito útil na solução de alguns problemas. Qual é a estrutura mais recomendada para o armazenamento desta contagem?
+
+my_list = [20, 20, 1, 2]
+
+count_elements = {
+    20: 2,
+    1: 1,
+    2: 2
+}
