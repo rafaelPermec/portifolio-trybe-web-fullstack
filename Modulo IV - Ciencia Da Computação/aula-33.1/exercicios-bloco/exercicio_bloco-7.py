5b004b8c0a2223226af67c4d2c733be8653064a5
rafaelPermec <geekchef.dev@gmail.com>
2022-09-19 13:17:54 -0300

    :construction: init (py): Primeiros passos em Python

--- Modulo IV - Ciencia Da Computação/aula-33.1/exercicios-bloco/exercicio_bloco-7.py
+++ Modulo IV - Ciencia Da Computação/aula-33.1/exercicios-bloco/exercicio_bloco-7.py
@@ -1,6 +1,7 @@
-# Exercício 1: Dada uma lista, descubra o menor elemento. Por exemplo, [5, 9, 3, 19, 70, 8, 100, 2, 35, 27] deve retornar 2.
+# Exercício 7: Dada uma lista, descubra o menor elemento. Por exemplo, [5, 9, 3, 19, 70, 8, 100, 2, 35, 27] deve retornar 2.
 
 number_list = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 9, 1, -40]
+
 
 def lowest_number (numbers):
   lowest = numbers[0]
@@ -9,4 +10,11 @@ def lowest_number (numbers):
       lowest = number
   return print(lowest)
 
+
 lowest_number(number_list)
+
+
+def minimum_function (numbers):
+  return print(min(numbers))
+
+minimum_function(number_list)
---
+++ Modulo IV - Ciencia Da Computação/aula-33.1/exercicios-bloco/exercicio_bloco-8.py
@@ -0,0 +1,1 @@
+# Exercício 8: Faça um programa que, dado um valor n qualquer, tal que n > 1, imprima na tela um triângulo retângulo com n asteriscos de base. Por exemplo, para n = 5 o triângulo terá 5 asteriscos na base:
