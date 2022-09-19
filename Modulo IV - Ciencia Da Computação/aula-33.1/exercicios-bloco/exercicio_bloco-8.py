cddbc48954b5e89bea3ea978473ed8525d929547
rafaelPermec <geekchef.dev@gmail.com>
2022-09-19 13:21:22 -0300

    :construction: init (py): Primeiros passos em Python

--- Modulo IV - Ciencia Da Computação/aula-33.1/exercicios-bloco/exercicio_bloco-8.py
+++ Modulo IV - Ciencia Da Computação/aula-33.1/exercicios-bloco/exercicio_bloco-8.py
@@ -1,1 +1,9 @@
 # Exercício 8: Faça um programa que, dado um valor n qualquer, tal que n > 1, imprima na tela um triângulo retângulo com n asteriscos de base. Por exemplo, para n = 5 o triângulo terá 5 asteriscos na base:
+
+def print_triangle (rows):
+  for row in range(1, rows + 1):
+    print(row * '*')
+
+
+print_triangle(10)
+
