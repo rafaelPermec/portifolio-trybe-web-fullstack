8b4e83ebb7609cb3a49947c2b9964a72ade4e261
rafaelPermec <geekchef.dev@gmail.com>
2022-09-19 10:33:18 -0300

    :construction: init (py): Primeiros passos em Python

---
+++ Modulo IV - Ciencia Da Computação/aula-33.1/exercicios-fixacao/exercicios-7.py
@@ -0,0 +1,5 @@
+# Exercício 7: Um conjunto ou set pode ser inicializado utilizando-se também o método set(). Inicialize uma variável com essa função var = set() e adicione seu nome ao conjunto utilizando um dos métodos vistos acima. Depois, imprima a variável e confira se o retorno é: {'seu_nome'}.
+
+minha_variavel = set({ 'Contrate_o_Rafael_Perdigão' })
+
+print(minha_variavel)
