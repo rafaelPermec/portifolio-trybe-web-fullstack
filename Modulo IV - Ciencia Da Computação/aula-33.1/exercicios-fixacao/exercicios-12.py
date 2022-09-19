4afaffb752ce4282db80cee00e37dacd5d6512fa
rafaelPermec <geekchef.dev@gmail.com>
2022-09-19 10:59:38 -0300

    :construction: init (py): Primeiros passos em Python

--- Modulo IV - Ciencia Da Computação/aula-33.1/exercicios-fixacao/exercicios-12.py
+++ Modulo IV - Ciencia Da Computação/aula-33.1/exercicios-fixacao/exercicios-12.py
@@ -1,11 +1,9 @@
 # Exercício 12: O Fatorial de um número inteiro é representado pela multiplicação de todos os números predecessores maiores que 0. Por exemplo, o fatorial de 5 é 120 pois 5! = 1 * 2 * 3 * 4 * 5. Escreva um código que calcule o fatorial de um número inteiro.
 
-number = 5
-counter = 1
+number = 10
 result = 1
 
-while counter <= number:
-  result *= counter
-  counter += 1
-  
+for index in range(1, number + 1):
+  result *= index
+
 print(result)
