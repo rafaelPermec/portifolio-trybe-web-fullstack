a7f900289bacd026d72b4e2e59032c4cad4208e8
rafaelPermec <geekchef.dev@gmail.com>
2022-09-19 11:20:30 -0300

    :construction: init (py): Primeiros passos em Python

---
+++ Modulo IV - Ciencia Da Computação/aula-33.1/exercicios-bloco/exercicio_bloco-1.py
@@ -0,0 +1,11 @@
+# Exercício 1: Crie uma função que receba dois números e retorne o maior deles.
+
+def bigger(number, other):
+    if other > number:
+        return print(other)
+    return print(number)
+
+bigger(1, 99)
+bigger(25, 5)
+bigger(42, 41)
+bigger(55, 54)
