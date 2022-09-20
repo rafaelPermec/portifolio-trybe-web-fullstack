6a72a1c53796fd22fd3e761212c00958d2f14d8c
rafaelPermec <geekchef.dev@gmail.com>
2022-09-20 18:24:37 -0300

    :construction: init (stdin e stdout): Entrada e Saida de Dados

---
+++ Modulo IV - Ciencia Da Computação/bloco-33/aula-33.2/exercicios-fixacao/exercicio_aula-2.py
@@ -0,0 +1,22 @@
+# Exercício 2: Escreva um programa que receba vários números naturais e calcule a soma desses valores. Caso algum valor da entrada seja inválido (por exemplo uma letra), uma mensagem deve ser exibida na saída de erros no seguinte formato: “Erro ao somar valores, {valor} é um valor inválido”. Ao final, você deve imprimir a soma dos valores válidos.
+
+numeros_usuario = input(
+  '''Por favor, coloque os valores que deseja somar, separados por espaço! 
+  Aqui: '''
+  )
+
+input_numeros_em_dict = numeros_usuario.split(' ')
+
+soma = 0
+
+for numero in input_numeros_em_dict:
+  if not numero.isdigit():
+    print(f'O caracter {numero} não é um número inteiro valido.')
+  else: 
+    soma += int(numero)
+
+print(f'''A soma dos valores que você digitou é: 
+
+TANTANTAAAAAAAN ! ~fogos de arificio~ ~xablau pra todo lado~
+
+{soma} !!!''')
