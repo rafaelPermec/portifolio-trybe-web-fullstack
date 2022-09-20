ffacd3e218bbdd1cf86dc16be96f364dd8bcb9c1
rafaelPermec <geekchef.dev@gmail.com>
2022-09-20 19:04:31 -0300

    :construction: init (stdin e stdout): Entrada e Saida de Dados

--- Modulo IV - Ciencia Da Computação/bloco-33/aula-33.2/exercicios-fixacao/exercicio_aula-3.py
+++ Modulo IV - Ciencia Da Computação/bloco-33/aula-33.2/exercicios-fixacao/exercicio_aula-3.py
@@ -1,3 +1,20 @@
+# Exercício 3: Dado um arquivo contendo estudantes e suas respectivas notas, escreva um programa que:
+
+# lê todas essas informações;
+# aplique um filtro, mantendo somente as pessoas que estão reprovadas;
+# escreva seus nomes em outro arquivo.
+# Considere que a nota miníma para aprovação é 6.
+
+# Arquivo:
+
+# Copiar
+# Marcos 10
+# Felipe 4
+# José 6
+# Ana 10
+# Maria 9
+# Miguel 5
+
 estudantes_em_recuperacao = []
 
 with open('./exemplo/alunos.txt') as file:
