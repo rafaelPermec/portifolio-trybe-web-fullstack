4a7d8c7b2baa65b202b8bed98a7a668c087deb6a
rafaelPermec <geekchef.dev@gmail.com>
2022-09-25 15:59:25 -0300

    :construction: feat: metodo try para captação de erros

--- Modulo IV - Ciencia Da Computação/bloco-33/aula-33.2/exercicios-fixacao/aula-pratica/games_report.py
+++ Modulo IV - Ciencia Da Computação/bloco-33/aula-33.2/exercicios-fixacao/aula-pratica/games_report.py
@@ -3,8 +3,18 @@ import json
 try:
     with open("video_games.json", mode="r") as file:
         video_games = json.load(file)
-        print(video_games[0]["Title"])
-except FileNotFoundError:
-    print("Arquivo não encontrado")
-except json.decoder.JSONDecodeError:
-    print("Formatação de JSON incorreta")
+except FileNotFoundError as apelido:
+    print(f"Arquivo não encontrado. Erro: {apelido}")
+except json.decoder.JSONDecodeError as apelido:
+    print(f"Formatação de JSON incorreta. Erro: {apelido}")
+# except:
+#     print("não deu certo! =/")
+finally:
+    print("Tudo que era para acontecer, aconteceu. Pode coletar o lixo aqui!")
+
+
+# Genres:
+game_genres = set()
+
+for game in video_games:
+    print(game["Title"])
