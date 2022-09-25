9ae0a29ecec4e2e94628652df8fbb46a0aa2c552
rafaelPermec <geekchef.dev@gmail.com>
2022-09-25 17:18:47 -0300

    :construction: feat: exercicios de estruturação de codigo em python

--- Modulo IV - Ciencia Da Computação/bloco-33/aula-33.2/exercicios-bloco/exercicio_bloco-2.py
+++ Modulo IV - Ciencia Da Computação/bloco-33/aula-33.2/exercicios-bloco/exercicio_bloco-2.py
@@ -13,6 +13,8 @@
 # o método choice:
 # random.choice(["word1", "word2", "word3"]) -> "word2".
 
+import random
+
 WORDS = [
   'cadê',
   'meu',
@@ -31,13 +33,31 @@ MAX_ATTEMPTS = 3
 
 
 def secret_word(words):
+    secret = random.choice(words)
+    scramble_secret = ''.join(random.sample(secret, len(secret)))
+    return secret, scramble_secret
 
 
 def guesses():
+    total_guesses = []
+    for _attempt in range(MAX_ATTEMPTS):
+        guess = input('Guess the word: ')
+        total_guesses.append(guess)
+    return total_guesses
 
 
 def check_results(secret, guesses):
+    if secret in guesses:
+        print(f'You won!! The secret word is: {secret}')
+    else:
+        print(f'You lose... The secret word is: {secret} =/')
 
 
 if __name__ == "__main__":
-
+    secret, scrambled_word = secret_word(WORDS)
+    print(
+      "Let's play a game! You have 3 turns to scramble back a word! \n"
+      f'Scrambled word is {scrambled_word}'
+      )
+    guesses = guesses()
+    check_results(secret, guesses)
