c00415e7a057d29fa450806602df3f2251c69df7
rafaelPermec <geekchef.dev@gmail.com>
2022-09-26 12:22:02 -0300

    :construction: feat: Primeiros testes unitários em python

--- Modulo IV - Ciencia Da Computação/bloco-33/aula-33.3/exercicios-bloco/tests/test_exercicios_bloco_4.py
+++ Modulo IV - Ciencia Da Computação/bloco-33/aula-33.3/exercicios-bloco/tests/test_exercicios_bloco_4.py
@@ -1,13 +1,15 @@
 from src.exercicios_bloco_4 import filter_valid_emails
 
+email = "username@website.com"
+
 
 def test_if_dont_have_emails_returns_empty_list():
     assert filter_valid_emails([]) == []
 
 
 def test_only_valid_emails():
-    emails = ["username@website.com"]
-    expected_emails = ["username@website.com"]
+    emails = [email]
+    expected_emails = [email]
     assert filter_valid_emails(emails) == expected_emails
 
 
@@ -18,6 +20,6 @@ def test_invalid_emails_should_be_filtered():
 
 
 def test_valid_and_invalid_emails_returns_only_valids():
-    emails = ["username@website.com", "inv*alid@website.com"]
-    expected_emails = ["username@website.com"]
+    emails = [email, "inv*alid@website.com"]
+    expected_emails = [email]
     assert filter_valid_emails(emails) == expected_emails
