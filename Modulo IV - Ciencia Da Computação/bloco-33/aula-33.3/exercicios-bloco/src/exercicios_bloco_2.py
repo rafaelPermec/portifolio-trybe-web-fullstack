4194af188109966c89d51c94731d8642183b4d7c
rafaelPermec <geekchef.dev@gmail.com>
2022-09-26 12:11:59 -0300

    :construction: feat: Primeiros testes unitários em python

--- Modulo IV - Ciencia Da Computação/bloco-33/aula-33.3/exercicios-bloco/src/exercicios_bloco_2.py
+++ Modulo IV - Ciencia Da Computação/bloco-33/aula-33.3/exercicios-bloco/src/exercicios_bloco_2.py
@@ -1,3 +1,31 @@
+# Exercício 2 Em alguns lugares é comum lembrar um número do telefone
+# associando seus dígitos a letras. Dessa maneira a expressão
+# MY LOVE significa 69 5683. Claro que existem alguns problemas,
+# uma vez que alguns números de telefone não formam uma palavra ou
+# uma frase e os dígitos 1 e 0 não estão associados a nenhuma letra.
+
+# Sua tarefa é ler uma expressão e encontrar o número de telefone
+# correspondente baseado na tabela abaixo. Uma expressão é composta
+# por letras maiúsculas (A-Z), hifens (-) e os números 1 e 0.
+
+# Letras  ->  Número
+# ABC     ->  2
+# DEF     ->  3
+# GHI     ->  4
+# JKL     ->  5
+# MNO     ->  6
+# PQRS    ->  7
+# TUV     ->  8
+# WXYZ    ->  9
+# Exemplo de entrada:
+
+# 1-HOME-SWEET-HOME
+# MY-MISERABLE-JOB
+# Saídas correspondentes:
+
+# 1-4663-79338-4663
+# 69-647372253-562
+
 def telephone_letters(str):
     if not 1 < len(str) <= 30:
         raise ValueError("Expression with invalid length")
