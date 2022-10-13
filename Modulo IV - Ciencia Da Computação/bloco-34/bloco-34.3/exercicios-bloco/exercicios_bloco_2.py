dd35608cac86ef51ebdd39094b3cd80931f86f4d
rafaelPermec <geekchef.dev@gmail.com>
2022-10-13 18:58:46 -0300

    :construction: feat: estudos de design patterns "decorators", "observer" e "factory"

--- Modulo IV - Ciencia Da Computação/bloco-34/bloco-34.3/exercicios-bloco/exercicios_bloco_2.py
+++ Modulo IV - Ciencia Da Computação/bloco-34/bloco-34.3/exercicios-bloco/exercicios_bloco_2.py
@@ -16,3 +16,92 @@
 # dinamicamente por composição ao objeto Alarme. Assim, quando o
 # despertador tocar todas as rotinas vinculadas serão disparadas
 # automaticamente.
+
+from abc import ABC, abstractmethod
+
+
+# Interface de funções
+class InterfaceFuncionalidades(ABC):
+    @abstractmethod
+    def acao(self):
+        pass
+
+
+class Alarme:
+    def __init__(self):
+        self._rotinas = []
+
+    def __str__(self) -> str:
+        print('''
+        Bom dia, humano! Minha missão é te agradar
+        - para depois conquistar o planeta! -
+        Agora que você acordou, vou fazer:
+        {self._rotinas}
+        ''')
+
+    def despertador(self):
+        print('Musica boa do Bad Bad Not Good')
+        self.realizar_todas_rotinas()
+
+    def add_rotina(self, rotina):
+        self._rotinas.append(rotina)
+
+    def realizar_todas_rotinas(self):
+        for rotina in self._rotinas:
+            rotina.acao()
+
+
+class FuncionalidadeCortinas(InterfaceFuncionalidades):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.add_rotina(self)
+
+    def acao(self):
+        print('Abrir as cortinas')
+
+
+class FuncionalidadeLuzes(InterfaceFuncionalidades):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.add_rotina(self)
+
+    def acao(self):
+        print('Acendendo as luzes')
+
+
+class FuncionalidadeCafe(InterfaceFuncionalidades):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.add_rotina(self)
+
+    def acao(self):
+        print('Prefere grãos torrados ou descafeinado?')
+
+
+class FuncionalidadePrepararBanho(InterfaceFuncionalidades):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.add_rotina(self)
+
+    def acao(self):
+        print('Chuá chuá, que bainho mais gostoso!')
+
+
+class FuncionalidadeNoticias(InterfaceFuncionalidades):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.add_rotina(self)
+
+    def acao(self):
+        print('Ligando no portal TrybeNews')
+
+
+if __name__ == "__main__":
+    alarme_manha = Alarme()
+    FuncionalidadeCortinas(alarme_manha)
+    FuncionalidadeLuzes(alarme_manha)
+    FuncionalidadeCafe(alarme_manha)
+    FuncionalidadePrepararBanho(alarme_manha)
+    FuncionalidadeNoticias(alarme_manha)
+
+    alarme_manha.despertador()
