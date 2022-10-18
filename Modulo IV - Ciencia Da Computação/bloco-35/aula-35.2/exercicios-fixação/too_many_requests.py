34d61bb3f2ec4359c1ba26b81cd7987a9dd3339b
rafaelPermec <geekchef.dev@gmail.com>
2022-10-17 23:41:04 -0300

    :construction: feat: Exercicios sobre web scrapping no python

--- Modulo IV - Ciencia Da Computação/bloco-35/aula-35.2/exercicios-fixação/too_many_requests.py
+++ Modulo IV - Ciencia Da Computação/bloco-35/aula-35.2/exercicios-fixação/too_many_requests.py
@@ -13,9 +13,9 @@ for _ in range(15):
 # e análise do tempo de resposta normal de uma requisição.
 try:
     # recurso demora muito a responder
-    response = requests.get("http://httpbin.org/delay/10", timeout=2)
+    response = requests.get("https://httpbin.org/delay/10", timeout=2)
 except requests.ReadTimeout:
     # vamos fazer uma nova requisição
-    response = requests.get("http://httpbin.org/delay/1", timeout=3)
+    response = requests.get("https://httpbin.org/delay/1", timeout=3)
 finally:
     print(response.status_code)
