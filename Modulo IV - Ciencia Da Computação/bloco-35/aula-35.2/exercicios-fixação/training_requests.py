f70d9a99b620e582fa1a6cddc68d3bc0f38970f1
rafaelPermec <geekchef.dev@gmail.com>
2022-10-17 23:13:45 -0300

    :construction: feat: Exercicios sobre web scrapping no python

--- Modulo IV - Ciencia Da Computação/bloco-35/aula-35.2/exercicios-fixação/training_requests.py
+++ Modulo IV - Ciencia Da Computação/bloco-35/aula-35.2/exercicios-fixação/training_requests.py
@@ -19,29 +19,29 @@ print(f"\n {SUBLINHADO}response.text:{RESET} \n {response.text}")
 print(f"\n {SUBLINHADO}response.content:{RESET} \n {response.content}")
 
 # Requisição do tipo post
-response = requests.post("http://httpbin.org/post", data="some content")
+response = requests.post("https://httpbin.org/post", data="some content")
 print(f"\n {SUBLINHADO}response.text:{RESET} \n {response.text}")
 
 # Requisição enviando cabeçalho (header)
 response = requests.get(
-  "http://httpbin.org/get",
+  "https://httpbin.org/get",
   headers={
     "Accept": "application/json"
     })
 print(f"\n {SUBLINHADO}response.text:{RESET} \n {response.text}")
 
 # Requisição a recurso binário
-response = requests.get("http://httpbin.org/image/png")
+response = requests.get("https://httpbin.org/image/png")
 print(f"\n {SUBLINHADO}response.content:{RESET} \n {response.content}")
 
 # Recurso JSON
-response = requests.get("http://httpbin.org/get")
+response = requests.get("https://httpbin.org/get")
 
 # Equivalente ao json.loads(response.content)
 print(f"\n {SUBLINHADO}response.json():{RESET} \n {response.json()}")
 
 # Podemos também pedir que a resposta lance uma exceção caso o status
 # não seja OK
-response = requests.get("http://httpbin.org/status/404")
+response = requests.get("https://httpbin.org/status/404")
 print(f"\n {SUBLINHADO}response.raise_for_status():{RESET}")
 response.raise_for_status()
