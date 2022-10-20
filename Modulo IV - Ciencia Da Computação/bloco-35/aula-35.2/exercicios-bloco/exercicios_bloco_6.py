ee9c78e8144cf4dd738266ae6c14fbd17d3b0f0a
rafaelPermec <geekchef.dev@gmail.com>
2022-10-20 16:54:33 -0300

    :construction: feat: Exercicios sobre web scrapping no python

---
+++ Modulo IV - Ciencia Da Computação/bloco-35/aula-35.2/exercicios-bloco/exercicios_bloco_6.py
@@ -0,0 +1,4 @@
+# Exercício 6: Escreva um programa que se conecte ao banco de dados
+# library e liste os livros da coleção books para uma determinada
+# categoria recebida por uma pessoa usuária. Somente o título dos livros
+# deve ser exibido.
